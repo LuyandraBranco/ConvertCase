73b5a8529a139791a91260a5c83a9dfac5ec456e
Luyandra_Branco <94245234+LuyandraBranco@users.noreply.github.com>
2023-05-05 14:19:42 +0100

    ContainerApp: Assistente de voz adicionado

--- src/components/TextAreaComponent/index.tsx
+++ src/components/TextAreaComponent/index.tsx
@@ -9,19 +9,23 @@ import { FiClipboard } from 'react-icons/fi';
 import { BiTrash } from 'react-icons/bi';
 import { HiVolumeUp } from 'react-icons/hi';
 
-export function TextAreaComponent({name, functionn, handleDelete, handleCopy, handlePlay}) {
+export function TextAreaComponent({ name, functionn, handleDelete, handleCopy, handlePlay }) {
 
     return (
         <>
-            <TextArea placeholder="Type or past your content here..." rows="6" value={name} onChange={functionn}>
+            <TextArea
+                placeholder="Type or past your content here..."
+                rows="6"
+                value={name}
+                onChange={functionn}>
             </TextArea>
             <Icons>
                 <Left>
-                    <HiVolumeUp onClick={handlePlay}/>
+                    <HiVolumeUp onClick={handlePlay} />
                 </Left>
                 <Right>
                     <FiClipboard onClick={handleCopy}/>
-                    <BiTrash onClick={handleDelete}/>
+                    <BiTrash onClick={handleDelete} />
                 </Right>
             </Icons>
         </>
